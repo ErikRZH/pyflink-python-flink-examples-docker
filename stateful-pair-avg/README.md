9f783b5b0c9c490b8d68ffc7aa4e4d9a62d62971
ErikRZH <supersamst.rydow@gmail.com>
2022-04-28 15:17:55 +0100

    stateful-pair-avg example added

---
+++ stateful-pair-avg/README.md
@@ -0,0 +1,48 @@
+## Stateful Function using Both APIs
+
+This example uses both API's and a Stateful streaming API function.
+
+The example consists of:  
+(1) Messages with a randomly selected 1 or 0 and a timestamp are generated and sent using kafka.  
+(2) Using Flink the average of every pair of two numbers, so one of (0,0), (1,0), (0,1), (1,1) is calculated. The average is converted to an integer, so you would expect the average to be 0 75% of the time and 1 25% of the time.
+(3) The processed data is stored using elasticsearch and may be visualised with Kibana. So one can check the distribution of the computed average.
+
+The Flink part of the programme follows the flow:
+* Sink read from Kafka into Table [Table API]
+* Datastream formed from table [Table API, Datastream API]
+* A stateful function is applied to each "Row" of the datastream (corresponding to table rows), this outputs an average integer and the timestamp of the second integer. [Datastream API, Custom stateful function]
+* A Table is formed from the transformed datastream [Datastream API, Table API]
+* Resulting table is written to the Elasticsearch sink. [Table API]
+
+To start the example, build the images:
+````commandline
+sudo docker-compose build
+````
+
+Then start the containers
+````
+sudo docker-compose up -d
+````
+
+To see the payment messages being sent you can run:
+````
+sudo docker-compose exec kafka kafka-console-consumer.sh --bootstrap-server kafka:9092 --topic random_binary_msg
+````
+To see the elasticsearch data (before the job there should be none) look at [http://localhost:9200/platform_avg_1/_search?pretty](http://localhost:9200/platform_avg_1/_search?pretty).
+
+Submit the payment processing job
+````commandline
+sudo docker-compose exec jobmanager ./bin/flink run -py /opt/stateful-pair-avg/demo_msg_proccessing.py -d
+````
+
+You can inspect the job at the various stages at:  
+Flink Web UI [http://localhost:8081](http://localhost:8081).   
+Elasticsearch [http://localhost:9200](http://localhost:9200).   
+Kibana [http://localhost:5601](http://localhost:5601).
+
+On Kibana there is a dashboard showing the spending of the different teams.
+
+To shut it down.
+```
+sudo docker-compose down
+```
---
+++ stateful-pair-avg/demo_msg_proccessing.py
@@ -0,0 +1,114 @@
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+from pyflink.common import WatermarkStrategy, Row
+from pyflink.common.typeinfo import Types
+from pyflink.datastream import StreamExecutionEnvironment, TimeCharacteristic, FlatMapFunction, RuntimeContext, MapFunction
+from pyflink.datastream.state import ValueStateDescriptor
+from pyflink.table import StreamTableEnvironment, DataTypes, EnvironmentSettings, Schema
+import logging
+
+class CountWindowAverage(FlatMapFunction):
+
+    def __init__(self):
+        self.sum = None
+
+    def open(self, runtime_context: RuntimeContext):
+        descriptor = ValueStateDescriptor(
+            "average",  # the state name
+            Types.PICKLED_BYTE_ARRAY()  # type information
+        )
+        self.sum = runtime_context.get_state(descriptor)
+
+    def flat_map(self, value):
+        # access the state value
+        logging.warning("RUNNING FUNCTION")
+        current_sum = self.sum.value() # Access the value of the state stored in self.sum
+        if current_sum is None:
+            current_sum = (0, 0) # Entries correspond to number of elements summed and the value of the sum respectively
+
+        # update the count
+        current_sum = (current_sum[0] + 1, current_sum[1] + value[1])
+
+        # update the state
+        self.sum.update(current_sum)
+
+        # if the count reaches 2, emit the average and clear the state
+        if current_sum[0] >= 2:
+            self.sum.clear()
+            yield Row(value[0], int(current_sum[1] / current_sum[0]))
+
+def log_processing():
+    env = StreamExecutionEnvironment.get_execution_environment()
+    t_env = StreamTableEnvironment.create(stream_execution_environment=env)
+    t_env.get_config().get_configuration().set_string("pipeline.name", "Extended Pipeline: Average Pairs of Binary Numbers")
+    t_env.get_config().get_configuration().set_boolean("python.fn-execution.memory.managed", True)
+
+    create_kafka_source_ddl = """
+            CREATE TABLE random_binary_msg (
+                createTime VARCHAR,
+                binaryInt TINYINT
+            ) WITH (
+              'connector' = 'kafka',
+              'topic' = 'random_binary_msg',
+              'properties.bootstrap.servers' = 'kafka:9092',
+              'properties.group.id' = 'test_3',
+              'scan.startup.mode' = 'latest-offset',
+              'value.format' = 'json'
+            )
+            """
+
+    create_es_sink_ddl = """
+            CREATE TABLE es_sink (
+                createTime VARCHAR,
+                averageInt INT
+            ) with (
+                'connector' = 'elasticsearch-7',
+                'hosts' = 'http://elasticsearch:9200',
+                'index' = 'platform_avg_1',
+                'sink.flush-on-checkpoint' = 'true',
+                'document-id.key-delimiter' = '$',
+                'sink.bulk-flush.max-size' = '42mb',
+                'sink.bulk-flush.max-actions' = '32',
+                'sink.bulk-flush.interval' = '1000',
+                'sink.bulk-flush.backoff.delay' = '1000',
+                'format' = 'json'
+            )
+    """
+    print("Printing result to stdout. Use --output to specify output path.")
+
+    # Sets up Table API calls
+    t_env.execute_sql(create_kafka_source_ddl)
+    t_env.execute_sql(create_es_sink_ddl)
+
+    # Create Table using the source
+    table = t_env.from_path("random_binary_msg")
+
+    # Convert Table to Datastream
+    ds = t_env.to_data_stream(table)
+
+    # Use Datastream API stateful function
+    ds = ds.key_by(lambda row: 1) \
+            .flat_map(CountWindowAverage(), output_type=Types.ROW([Types.STRING(), Types.INT()])) \
+
+    # Convert Datastream back to table
+    table_out = t_env.from_data_stream(ds).alias("binaryInt")
+    # Write to sink
+    table_out.execute_insert("es_sink")
+
+if __name__ == '__main__':
+    log_processing()
